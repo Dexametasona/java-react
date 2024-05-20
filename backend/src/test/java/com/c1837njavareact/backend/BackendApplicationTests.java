c4ef10ba96cdbf74353d6ad3708a66a81fc1f3db
Joshua Frías <metatron1495@gmail.com>
2024-05-20 15:49:32 -0600

    Add: Primera estructura de backend y readme

---
+++ backend/src/test/java/com/c1837njavareact/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c1837njavareact.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
