f29443a339ebc9eff01a5a4b3508f88107ce867a
Dexametasona <machicadogomezalexander@gmail.com>
2024-05-22 18:50:15 -0500

    fix: fixing duplicated dependencies

--- backend/src/main/java/com/c1837njavareact/backend/model/dto/StackDtoRes.java
+++ backend/src/main/java/com/c1837njavareact/backend/model/dto/StackDtoRes.java
@@ -1,7 +1,5 @@
 package com.c1837njavareact.backend.model.dto;
 
-import com.c1837njavareact.backend.model.entities.Stack;
-
 public record StackDtoRes (
   int id_stack,
   String name
--- backend/src/main/java/com/c1837njavareact/backend/model/entities/Stack.java
+++ backend/src/main/java/com/c1837njavareact/backend/model/entities/Stack.java
@@ -1,7 +1,6 @@
 package com.c1837njavareact.backend.model.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.HashSet;
